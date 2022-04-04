07710aabf31156962eac24838a911c5b833cda24
Minh Tran <tranhuuminh3124@gmail.com>
2022-04-04 14:31:09 +0700

    export create_db functioon

--- src/database.py
+++ src/database.py
@@ -5,16 +5,28 @@ from config import settings
 
 SQLALCHEMY_DATABASE_URL = settings.db_url
 
+# Reading more about create engine at here
+#https://docs.sqlalchemy.org/en/14/core/engines.html
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
     echo=True,
     connect_args={'check_same_thread': False}
 )
 
+# https://docs.sqlalchemy.org/en/14/orm/session.html
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
 
+# https://docs.sqlalchemy.org/en/14/orm/declarative_tables.html
 Base = declarative_base()
+
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
