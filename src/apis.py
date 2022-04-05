95d45b9a5f9d6188e51ee11d17e4100c9ac4e048
Minh Tran <tranhuuminh3124@gmail.com>
2022-04-05 18:01:09 +0700

    Remove file not used

--- src/apis.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from routers import auth, todos, users
-
-router = APIRouter()
-
-router.include_router(auth.router, prefix="", tags=["Authentication"])
-router.include_router(users.router, prefix="/users", tags=["Users"])
-router.include_router(todos.router, prefix="/todos", tags=["Todos"])
