85dc10333339d5ab4a97a582ee714637314dce8b
Minh Tran <tranhuuminh3124@gmail.com>
2022-04-06 15:04:21 +0700

    Add new field to User modal

--- src/models/user.py
+++ src/models/user.py
@@ -12,6 +12,8 @@ class Users(Base):
     username = Column(String, unique=True, index=True)
     first_name = Column(String)
     last_name = Column(String)
-    hashed_password = Column(String)
+    hashed_password = Column(String, nullable=False)
     is_active = Column(Boolean, default=True)
+    is_superuser = Column(Boolean, default=False)
+
     todos = relationship('Todos', back_populates='owner')
